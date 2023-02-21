94ba69f436985f94b06a60a5c3af0c55e53d5ce8
hwangblood <hwangblood@gmail.com>
2023-02-21 11:54:34 +0800

    feat(backend): User Route

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,18 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
-import "dotenv/config";
-import { log } from "console";
+import "dotenv/config.js";
+
+import routes from "./src/routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser);
+app.use(cookieParser());
+
+app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 
@@ -20,12 +23,12 @@ const server = http.createServer(app);
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
-    log("MongoDB connected");
+    console.log("MongoDB connected");
     server.listen(port, () => {
-      log(`Server is listening on port ${port}`);
+      console.log(`Server is listening on port ${port}`);
     });
   })
   .catch((err) => {
-    log({ err });
+    console.log({ err });
     process.exit(1);
   });
