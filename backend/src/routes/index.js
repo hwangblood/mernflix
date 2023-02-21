0d98ffa9fb3cf648946414cb34caab4f78db7be9
hwangblood <hwangblood@gmail.com>
2023-02-21 19:57:41 +0800

    chore(backend): Media Route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import userRoute from "./user.route.js";
+import mediaRoute from "./media.route.js";
 
 const router = express.Router();
 
 router.use("/user", userRoute);
+
+router.use("/:mediaType", mediaRoute);
 
 export default router;
