52c3854381c2d4a279c5917328ddce48dc6037f3
hwangblood <hwangblood@gmail.com>
2023-02-21 19:58:29 +0800

    feat(backend): Person Route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import userRoute from "./user.route.js";
 import mediaRoute from "./media.route.js";
+import personRoute from "./person.route.js";
 
 const router = express.Router();
 
 router.use("/user", userRoute);
 
 router.use("/:mediaType", mediaRoute);
+
+router.use("/person", personRoute);
 
 export default router;
