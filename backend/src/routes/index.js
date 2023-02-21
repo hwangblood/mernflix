93a2a3f864bc7213e6870a9542fa478a3c2bb583
hwangblood <hwangblood@gmail.com>
2023-02-21 19:58:57 +0800

    feat(backend): Review Route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoute from "./user.route.js";
 import mediaRoute from "./media.route.js";
 import personRoute from "./person.route.js";
+import reviewRoute from "./review.route.js";
 
 const router = express.Router();
 
@@ -10,5 +11,7 @@ router.use("/user", userRoute);
 router.use("/:mediaType", mediaRoute);
 
 router.use("/person", personRoute);
+
+router.use("/reviews", reviewRoute);
 
 export default router;
