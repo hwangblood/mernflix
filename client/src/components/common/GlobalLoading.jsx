e0f7dc43f7a4166855dc9ba3d5ad1b02eaa41b62
hwangblood <hwangblood@gmail.com>
2023-04-24 21:30:57 +0800

    feat(client): Global Loading

---
+++ client/src/components/common/GlobalLoading.jsx
@@ -0,0 +1,52 @@
+import { Paper, Box, LinearProgress, Toolbar } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Logo from "./Logo";
+
+const GlobalLoading = () => {
+  const { globalLoading } = useSelector((state) => state.globalLoading);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (globalLoading) {
+      setIsLoading(globalLoading);
+    } else {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+  }, [globalLoading]);
+  return (
+    <>
+      <Paper
+        sx={{
+          opacity: isLoading ? 1 : 0,
+          pointerEvents: "none",
+          transition: "all .3s ease",
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 999,
+        }}
+      >
+        <Toolbar />
+
+        <LinearProgress />
+
+        <Box
+          sx={{
+            position: "abasolute",
+            top: "50%",
+            left: "50%",
+            transform: "transform(-50%, -50%)",
+          }}
+        >
+          <Logo />
+        </Box>
+      </Paper>
+    </>
+  );
+};
+
+export default GlobalLoading;
---
+++ client/src/components/common/Logo.jsx
@@ -0,0 +1,19 @@
+import { Typography, useTheme } from "@mui/material";
+
+const Logo = () => {
+  const theme = useTheme();
+  return (
+    <Typography fontWeight={"700"} fontSize={"1.7rem"}>
+      MERN
+      <span
+        style={{
+          color: theme.palette.primary.main,
+        }}
+      >
+        Flix
+      </span>
+    </Typography>
+  );
+};
+
+export default Logo;
