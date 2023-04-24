bd27a18a2d522d8d49b3f29f653c4d89ff14ebd8
hwangblood <hwangblood@gmail.com>
2023-04-24 19:24:00 +0800

    feat(client): Make Theme Configs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  return <div>App</div>;
+  const { themeMode } = useSelector((state) => state.themeMode);
+  return (
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* config tiastify */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      {/* mui reset css */}
+      <CssBaseline />
+    </ThemeProvider>
+  );
 };
 
 export default App;
