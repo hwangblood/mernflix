0c95d4d8df8ad3d533ed33a6874b93ace52fcd46
hwangblood <hwangblood@gmail.com>
2023-05-11 16:51:04 +0800

    feat(client): Make Movie Slide

--- client/src/components/common/Container.jsx
+++ client/src/components/common/Container.jsx
@@ -16,23 +16,21 @@ const Container = ({ header, children }) => {
             sx={{
               position: "relative",
               paddingX: { xs: "20px", md: 0 },
+              maxWidth: "1366px",
               marginX: "auto",
               width: "100%",
               "&::before": {
                 content: '""',
-                position: "relative",
-                left: {
-                  xs: "20px",
-                  md: "0",
-                },
-                top: "10-0%",
+                position: "absolute",
+                left: { xs: "20px", md: "0" },
+                top: "100%",
                 height: "5px",
                 width: "100px",
                 backgroundColor: "primary.main",
               },
             }}
           >
-            <Typography variant="h5" fontWeight={700}>
+            <Typography variant="h5" fontWeight="700" textTransform="uppercase">
               {header}
             </Typography>
           </Box>
---
+++ client/src/components/common/MediaSlide.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { SwiperSlide } from "swiper/react";
+import mediaApi from "../../api/modules/media.api";
+import AutoSwiper from "./AutoSwiper";
+import { toast } from "react-toastify";
+
+const MediaSlide = ({ mediaType, mediaCategory }) => {
+  const [medias, setMedias] = useState([]);
+
+  useEffect(() => {
+    const getMedias = async () => {
+      const { response, err } = await mediaApi.getList({
+        mediaType,
+        mediaCategory,
+        page: 1,
+      });
+
+      if (response) setMedias(response.results);
+      if (err) toast.error(err.message);
+    };
+
+    getMedias();
+  }, [mediaType, mediaCategory]);
+
+  return (
+    <AutoSwiper>
+      {medias.map((media, index) => (
+        <SwiperSlide key={index}>{"Slide Item"}</SwiperSlide>
+      ))}
+    </AutoSwiper>
+  );
+};
+
+export default MediaSlide;
