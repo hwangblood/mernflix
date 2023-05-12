66ad8868c3390a5d20d70da7577410946cbb5004
hwangblood <hwangblood@gmail.com>
2023-05-12 14:28:43 +0800

    feat(client): Make Movie Item

--- client/src/components/common/MediaSlide.jsx
+++ client/src/components/common/MediaSlide.jsx
@@ -3,6 +3,7 @@ import { SwiperSlide } from "swiper/react";
 import mediaApi from "../../api/modules/media.api";
 import AutoSwiper from "./AutoSwiper";
 import { toast } from "react-toastify";
+import MediaItem from "./MediaItem";
 
 const MediaSlide = ({ mediaType, mediaCategory }) => {
   const [medias, setMedias] = useState([]);
@@ -25,7 +26,9 @@ const MediaSlide = ({ mediaType, mediaCategory }) => {
   return (
     <AutoSwiper>
       {medias.map((media, index) => (
-        <SwiperSlide key={index}>{"Slide Item"}</SwiperSlide>
+        <SwiperSlide key={index}>
+          <MediaItem media={media} mediaType={mediaType} />
+        </SwiperSlide>
       ))}
     </AutoSwiper>
   );
